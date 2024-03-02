ca6c538897120fed5ff6e5eb9fe013b98c2ac913
ducbui2005hadd <23010513@st.phenikaa-uni.edu.vn>
2024-03-02 10:25:56 +0700

    Ex9

--- src/main/java/com/mycompany/app/Week1_Code3/Ex6.java
+++ src/main/java/com/mycompany/app/Week1_Code3/Ex6.java
@@ -18,4 +18,9 @@ public class Ex6 {
 		else
 		return -1;
 		}
+     public static void main(String[] args) {
+        System.out.println(checkReturn(10, 5, 5));
+        System.out.println(checkReturn(5, 15, 10));
+        System.out.println(checkReturn(5, 5, 5));
+    }
 }
